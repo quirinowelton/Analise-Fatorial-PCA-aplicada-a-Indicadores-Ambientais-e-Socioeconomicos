45b223089c9acf4d86563554c0e35c3d1519b20b
Welton Quirino Pereira <167942541+quirinowelton@users.noreply.github.com>
2026-01-28 19:26:39 -0300

    Add files via upload

---
+++ pca_aplicada_a_indicadores_ambientais_e_socioeconômicos_.py
@@ -0,0 +1,227 @@
+
+import pandas as pd
+import numpy as np
+from factor_analyzer import FactorAnalyzer
+from factor_analyzer.factor_analyzer import calculate_bartlett_sphericity
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+import seaborn as sns
+import plotly.graph_objects as go
+import shapefile as shp
+import warnings
+warnings.filterwarnings("ignore")
+
+## 📥 Importação da Base de Dados
+atlas = pd.read_excel("atlas_ambiental.xlsx")
+print(atlas.info())
+
+# Estatísticas descritiva das variáveis
+atlas.drop(columns=['cód_ibge']).describe()
+
+#Separando somente as variáveis quantitativas do banco de dados
+atlas_pca = atlas.drop(columns=['cód_ibge', 'distritos'])
+
+#Matriz de correlações entre as variáveis
+corr = atlas_pca.corr()
+
+# Gráfico interativo
+fig = go.Figure()
+
+fig.add_trace(
+    go.Heatmap(
+        x = corr.columns,
+        y = corr.index,
+        z = np.array(corr),
+        text = corr.values,
+        texttemplate = '%{text:.2f}',
+        colorscale = 'viridis'))
+
+fig.update_layout(
+    height = 600,
+    width = 600,
+    yaxis = dict(autorange='reversed'),
+    title = {'text':'Matriz de Correlações', 'x':0.50})
+fig.show()
+
+#Teste de Esfericidade de Bartlett
+bartlett, p_value = calculate_bartlett_sphericity(atlas_pca)
+print(f'Qui² Bartlett: {round(bartlett, 2)}')
+print(f'p-valor: {round(p_value, 4)}')
+
+#Definindo a PCA (procedimento inicial com todos os fatores possíveis)
+fa = FactorAnalyzer(n_factors=9, method='principal', rotation=None).fit(atlas_pca)
+
+#Obtendo os eigenvalues (autovalores)
+autovalores = fa.get_eigenvalues()[0]
+print(autovalores)
+
+
+# Parametrizando a PCA para os fatores derivados de autovalores > 1
+fa = FactorAnalyzer(n_factors=2, method='principal', rotation=None).fit(atlas_pca)
+
+# Eigenvalues, variâncias e variâncias acumuladas
+autovalores_fatores = fa.get_factor_variance()
+tabela_eigen = pd.DataFrame(autovalores_fatores)
+tabela_eigen.columns = [f"Fator {i+1}" for i, v in enumerate(tabela_eigen.columns)]
+tabela_eigen.index = ['Autovalor','Variância', 'Variância Acumulada']
+tabela_eigen = tabela_eigen.T
+
+print(tabela_eigen)
+
+# Gráfico da variância acumulada dos componentes principais
+plt.figure(figsize=(12,8), dpi=600)
+ax = sns.barplot(x=tabela_eigen.index, y=tabela_eigen['Variância'], hue=tabela_eigen.index, palette='rocket', data=tabela_eigen)
+for container in ax.containers:
+    labels = [f"{v*100:.2f}%" for v in container.datavalues]
+    ax.bar_label(container, labels=labels)
+ax.yaxis.set_major_formatter(mtick.FuncFormatter(lambda x, pos: f"{x*100:.0f}%"))
+plt.title("Fatores Extraidos", fontsize=16)
+plt.xlabel(f"{tabela_eigen.shape[0]} fatores que explicam {round(tabela_eigen['Variância'].sum()*100,2)}% da variância", fontsize=12)
+plt.ylabel("Variância explicada", fontsize=12)
+plt.show()
+
+#Determinando as cargas fatoriais
+cargas_fatoriais = fa.loadings_
+
+tabela_cargas = pd.DataFrame(cargas_fatoriais)
+tabela_cargas.columns = [f"Fator {i+1}" for i, v in enumerate(tabela_cargas.columns)]
+tabela_cargas.index = atlas_pca.columns
+print(tabela_cargas)
+
+#Gráfico das cargas fatoriais (loading plot)
+plt.figure(figsize=(12,8), dpi=600)
+tabela_cargas_chart = tabela_cargas.reset_index()
+plt.scatter(tabela_cargas_chart['Fator 1'], tabela_cargas_chart['Fator 2'], s=50, color='red')
+
+def label_point(x, y, val, ax):
+    a = pd.concat({'x': x, 'y': y, 'val': val}, axis=1)
+    for i, point in a.iterrows():
+        ax.text(point['x'] + 0.05, point['y'], point['val'])
+
+label_point(x = tabela_cargas_chart['Fator 1'],
+            y = tabela_cargas_chart['Fator 2'],
+            val = tabela_cargas_chart['index'],
+            ax = plt.gca())
+
+plt.axhline(y=0, color='grey', ls='--')
+plt.axvline(x=0, color='grey', ls='--')
+plt.ylim([-1.1,1.1])
+plt.xlim([-1.1,1.1])
+plt.title("Loading Plot", fontsize=16)
+plt.xlabel(f"Fator 1: {round(tabela_eigen.iloc[0]['Variância']*100,2)}% de variância explicada", fontsize=12)
+plt.ylabel(f"Fator 2: {round(tabela_eigen.iloc[1]['Variância']*100,2)}% de variância explicada", fontsize=12)
+plt.show()
+
+#Determinando as comunalidades
+comunalidades = fa.get_communalities()
+
+tabela_comunalidades = pd.DataFrame(comunalidades)
+tabela_comunalidades.columns = ['Comunalidades']
+tabela_comunalidades.index = atlas_pca.columns
+print(tabela_comunalidades)
+
+#Extração dos fatores para as observações do banco de dados
+fatores = pd.DataFrame(fa.transform(atlas_pca))
+fatores.columns =  [f"Fator {i+1}" for i, v in enumerate(fatores.columns)]
+
+#Adicionando os fatores ao banco de dados
+atlas = pd.concat([atlas.reset_index(drop=True), fatores], axis=1)
+atlas
+
+#Identificando os scores fatoriais
+scores = fa.weights_
+
+tabela_scores = pd.DataFrame(scores)
+tabela_scores.columns = [f"Fator {i+1}" for i, v in enumerate(tabela_scores.columns)]
+tabela_scores.index = atlas_pca.columns
+print(tabela_scores)
+
+#Considere que o primeiro fator seja adequado para o ranking de distritos
+
+dados_mapa = atlas[['cód_ibge', 'distritos', 'Fator 1']].sort_values(by=['cód_ibge'], ascending = True).reset_index(drop=True)
+
+#Importando o mapa da cidade de São Paulo
+sf = shp.Reader("DEINFO_DISTRITO")
+def read_shapefile(sf):
+    fields = [x[0] for x in sf.fields][1:]
+    records = [y[:] for y in sf.records()]
+    shps = [s.points for s in sf.shapes()]
+    df = pd.DataFrame(columns=fields, data=records)
+    df = df.assign(coords=shps)
+    return df
+
+dist = read_shapefile(sf)
+
+# Organizando o banco de dados das coordenadas
+dist['COD_DIST'] = pd.to_numeric(dist['COD_DIST'])
+dist = dist.sort_values(by=['COD_DIST'], ascending = True).reset_index(drop=True)
+
+# Gerando o mapa da cidade de SP com distritos delimitados
+def plot_map(df, x_lim = None, y_lim = None, figsize = (8,11)):
+
+    plt.figure(figsize = figsize)
+    id=0
+    for coordinates in df.coords:
+        x = [i[0] for i in coordinates]
+        y = [i[1] for i in coordinates]
+        plt.plot(x, y, 'k')
+
+        if (x_lim == None) & (y_lim == None):
+            x0 = np.mean(x)
+            y0 = np.mean(y)
+            plt.text(x0, y0, id, fontsize=5, ha='center', va='center')
+        id = id+1
+
+    if (x_lim != None) & (y_lim != None):
+        plt.xlim(x_lim)
+        plt.ylim(y_lim)
+
+    plt.axis('off')
+
+plot_map(dist)
+
+# Definindo as cores
+# dividir os valores do fator em 6 faixas
+def calc_color(data):
+        new_data = pd.qcut(data, 6, labels=list(range(6)))
+        paleta = sns.color_palette('YlOrBr', n_colors=6)
+        color_sq = paleta.as_hex()
+        color_ton = []
+        for val in new_data:
+            color_ton.append(color_sq[val])
+        return color_ton
+
+#Plotando o mapa com as cores
+def plot_map_fill_multiples_ids_3(df, title, distrito_id, color_ton, x_lim = None, y_lim = None, figsize = (8,11)):
+
+    plt.figure(figsize = figsize)
+    fig, ax = plt.subplots(figsize = figsize)
+    fig.suptitle(title, fontsize=16)
+
+    for coordinates in df.coords:
+        x = [i[0] for i in coordinates]
+        y = [i[1] for i in coordinates]
+        plt.plot(x, y, 'k')
+
+    for id in distrito_id:
+        shape_ex = df.coords[id]
+        x_lon = np.zeros((len(shape_ex),1))
+        y_lat = np.zeros((len(shape_ex),1))
+        for ip in range(len(shape_ex)):
+            x_lon[ip] = shape_ex[ip][0]
+            y_lat[ip] = shape_ex[ip][1]
+        ax.fill(x_lon,y_lat,  color_ton[distrito_id.index(id)]);
+        x0 = np.mean(x_lon)
+        y0 = np.mean(y_lat)
+        plt.text(x0, y0, id, fontsize=6, ha='center', va='center')
+
+    if (x_lim != None) & (y_lim != None):
+        plt.xlim(x_lim)
+        plt.ylim(y_lim)
+
+    plt.axis('off')
+
+distritos = list(np.arange(96)) # id_distritos
+data = list(dados_mapa['Fator 1']) # indicador socioeconômico (fator)
+color_ton = calc_color(data) # tonalidade de cores
+plot_map_fill_multiples_ids_3(dist, 'Indicador Socioeconômico', distritos, color_ton)
